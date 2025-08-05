40c9e259a4ca516d2d62f5789bce3673e73f8ecb
emilia <emi1213galarza@gmail.com>
2025-08-05 12:57:47 -0500

    Add user delete mutation into composables

---
+++ src/features/usuarios/composables/mutations/use-delete-usuario.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/vue-query";
+import { useQueryClient } from "node_modules/@tanstack/vue-query/build/modern/useQueryClient";
+import { useToast } from "vue-toastification";
+import { UsuarioDataSource } from "../../services/datasource";
+import { QUERY_KEY } from "@/shared/composables/query-key";
+
+export function useDeleteUsuario() {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+  return useMutation({
+    mutationFn: (id: number) => new UsuarioDataSource().delete(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.USUARIOS] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.USUARIOS_FILTROS] });
+      toast.success("Usuario eliminado exitosamente.");
+    },
+    onError: () => {
+      toast.error("Error al eliminar el usuario.");
+    },
+  });
+}
