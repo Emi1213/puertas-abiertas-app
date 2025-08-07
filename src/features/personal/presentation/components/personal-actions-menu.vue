3cba005c558a8171574001ff8987928c1ffbabb1
emilia <emi1213galarza@gmail.com>
2025-08-07 14:22:04 -0500

    Modified action menu

--- src/features/personal/presentation/components/personal-actions-menu.vue
+++ src/features/personal/presentation/components/personal-actions-menu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
+import { Edit, Trash2 } from "lucide-vue-next";
 import type { IPersonal } from "../../interfaces/IPersonal";
 
 defineProps<{
@@ -17,7 +18,7 @@ defineProps<{
       class="text-blue-600 hover:text-blue-700"
       @click.stop="onEdit"
     >
-      Editar
+      <Edit class="h-4 w-4" />
     </Button>
 
     <Button
@@ -26,7 +27,7 @@ defineProps<{
       class="text-red-600 hover:text-red-700"
       @click.stop="onDelete"
     >
-      Eliminar
+      <Trash2 class="h-4 w-4" />
     </Button>
   </div>
 </template>
